9ba2ae0d0526a72bd5b63cfef1e41ea5534fcd0d
francloz <francloz@ucm.es>
2018-03-09 17:10:15 +0100

    Base classes

--- src/tp/Cruce.java
+++ src/tp/Cruce.java
@@ -1,5 +1,24 @@
 package tp;
 
+import java.util.ArrayList;	
+	
 public class Cruce {
-
+	ArrayList<Vehiculo> listaVehiculos = new ArrayList<>();
+	boolean green;
+	
+	public boolean isGreen() {
+		return green;
+	}
+	public void setGreen(boolean green) {
+		this.green = green;
+	}
+	public void entraVehiculo(Vehiculo vehiculo) {
+		listaVehiculos.add(vehiculo);
+	}
+	public void saleVehiculo(Vehiculo vehiculo) {
+		if(!listaVehiculos.isEmpty()) {
+			Vehiculo v = listaVehiculos.get(0);
+			v.getSigCruce().entraVehiculo(v);
+		}
+	}
 }
