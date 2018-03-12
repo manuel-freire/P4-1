3969b17b0947cd2a882a63d6a84dca97a002f16c
Francloz <33878222+Francloz@users.noreply.github.com>
2018-03-12 16:03:46 +0100

    Revert "Events, EventBuilder"

--- src/tp/model/TrafficSimulator.java
+++ src/tp/model/TrafficSimulator.java
@@ -1,8 +1,36 @@
 package tp.model;
 
-import tp.model.simulatedObjects.Junction;
-import tp.model.simulatedObjects.Road;
+import java.util.List;
+
+import tp.model.simulatedObjects.*;
+import tp.model.events.Event;
 
 public class TrafficSimulator {
+	private List<Junction> junctions;
+	private List<Road> roads;
+	private List<Vehicle> vehicles;
+	private List<Event> events;
+	int contadorTiempo, pasosSimulacion;
 	
+	public void execute() {
+		int limiteTiempo = this.contadorTiempo + pasosSimulacion - 1; 
+		while (this.contadorTiempo <= limiteTiempo) { 
+			for(Event e: events) {
+				if(e.getTime() <= contadorTiempo)
+					e.execute();
+			}
+			for(Road r : roads)
+				r.advance();
+			for(Junction j : junctions)
+				j.advance();
+			contadorTiempo++; 
+			// 5. esciribir un informe en OutputStream en caso de que no sea null 
+			for(Road r : roads)
+				r.generateReport();
+			for(Junction j : junctions)
+				j.generateReport();
+			for(Vehicle v : vehicles)
+				v.generateReport();
+		} 
+	}
 }
