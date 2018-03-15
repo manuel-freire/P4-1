1a0d1fe39b1cc255ff2df6787a48704ebc0ea0d4
nava98 <32801954+nava98@users.noreply.github.com>
2018-03-16 00:51:37 +0100

    Events_again

---
+++ src/tp/events/EventsList.java
@@ -0,0 +1,10 @@
+package tp.control.events;
+
+import java.util.ArrayList;
+
+public class EventsList {
+	public ArrayList<Event>EventsList;
+	public EventsList() {
+		EventsList=new ArrayList<Event>();
+	}
+}
